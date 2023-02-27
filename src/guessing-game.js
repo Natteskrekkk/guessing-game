9201e4df019af167bc95babc81bc24588e78bab2
Timofey Gribanov <timofey.gribanov@gmail.com>
2023-02-27 14:15:10 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,69 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
+    min;
+    max;
+    middle;
+
+    /**
+     * this method accepts min and max value of range of number to guess
+     * @param {*} min 
+     * @param {*} max 
+     */
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
+    /**
+     * this method returns solution candidate (you make an assumption based on
+     * range and previous assumptions)
+     */
     guess() {
-
+        this.middle = Math.round((this.min + this.max) / 2);
+        return this.middle;
     }
 
+    /**
+     * this method is called if prev call of guess() returned number which is 
+     * greater than answer
+     */
     lower() {
-
+        this.max = this.middle;
+        return this.guess();
     }
 
+    /**
+     * this method is called if prev call of guess() returned number which is 
+     * lower than answer
+     */
     greater() {
-
+        this.min = this.middle;
+        return this.guess();
     }
 }
 
 module.exports = GuessingGame;
+
+
+// function findNum(min, max, num) {
+//     let middle = Math.floor((min + max) / 2);
+//     if (middle > num) {
+//         max = middle - 1;
+//         return findNum(min, max, num);
+//     } else if (middle < num) {
+//         min = middle + 1;
+//         return findNum(min, max, num);
+//     } else {
+//         return middle === num;
+//     }
+// }
+
+// findNum(0, 600, 30);
+
+
+
+
+
+
+
